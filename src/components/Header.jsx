8b56e3377a5d074b50041e0a23649345762981b7
Ahmed-abdisalaam-ahmed <eldinshehab87@gmail.com>
2025-12-20 13:17:54 +0000

    finished the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import { FcCommandLine } from "react-icons/fc";
 import { FaUser } from "react-icons/fa";
 import { CiMenuBurger } from "react-icons/ci";
 import { IoMdClose } from "react-icons/io";
+import { SiTrueup } from "react-icons/si";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -134,25 +135,75 @@ const Header = () => {
         </div>
 
         {/* mobile  */}
-        {
-          isMenuOpen && (
-                    <div className="pt-2 pb-3 space-y-1">
-          <Link
-            to="/"
-            className="block pl-3 pr-4 py-2 border-l-4 border-orange-500 text-base font-medium text-orange-700 bg-orange-50"
-          >
-            Home
-          </Link>
-          <Link
-            to="/articles"
-            className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
-          >
-            Articles
-          </Link>
-        </div>
-          )
-        }
+        {isMenuOpen && (
+          <div className="sm:hidden py-4">
+            <div className="pt-2 pb-3 space-y-1">
+              <Link
+                to="/"
+                className="block pl-3 pr-4 py-2 border-l-4 border-orange-500 text-base font-medium text-orange-700 bg-orange-50"
+              >
+                Home
+              </Link>
+              <Link
+                to="/articles"
+                className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+              >
+                Articles
+              </Link>
+            </div>
 
+            {isLoggedIn && (
+              <>
+                <Link
+                  to="/editor"
+                  className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+                >
+                  Write
+                </Link>
+                <Link
+                  to="/manage-articles"
+                  className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+                >
+                  My Articles
+                </Link>
+                <Link
+                  to="/profile"
+                  className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+                >
+                  Profile
+                </Link>
+                <button
+                  onClick={() => {
+                    alert("hhhh");
+                  }}
+                  className="block w-full text-left pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+                >
+                  Sign Out
+                </button>
+              </>
+            )}
+
+            {/* if is not logged in */}
+
+            {!isLoggedIn && (
+              <>
+                <Link
+                  to="/signin"
+                  className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+                >
+                  Sign In
+                </Link>
+                <Link
+                  to="/signup"
+                  className="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"
+                >
+                  Sign Up
+                </Link>
+              </>
+            )}
+
+          </div>
+        )}
       </div>
     </header>
   );
