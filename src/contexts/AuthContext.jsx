1e27f5c1cdb0b0592586a8d2d3849ddc5e344496
Ahmed-abdisalaam-ahmed <eldinshehab87@gmail.com>
2025-12-23 17:05:34 +0000

    commit the AuthContxt

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { getUserProfile, onAuthchange } from "../lib/Auth";
 
 const AuthContext = createContext(null);
 
@@ -7,4 +8,27 @@ export function AuthcontextProvider({children}){
     const [user, setUser] = useState(null)
     const [profile, setProfile] = useState(null)
     const [loading,setLoading] = useState(true)
+
+    useEffect(() => {
+
+     const cleanup = onAuthchange(async (user) => {
+            setUser(user);
+
+        if(user){
+
+            try {
+                const userProfile = await getUserProfile(user.id);
+                setProfile(userProfile)
+            } catch (error) {
+                    console.error("Error faetching user profile: ", error);
+            }
+        }else{
+            setProfile(null)
+        }
+        setLoading(false)
+    })
+
+    return cleanup;
+
+    },[])
 }
