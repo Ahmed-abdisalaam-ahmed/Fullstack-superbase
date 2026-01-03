b983c658c14ffa942d6488adf85caaa8072814fd
Ahmed-abdisalaam-ahmed <shihabeldin70@gmail.com>
2026-01-03 17:27:39 +0000

    finished the articles

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { useAuth } from '../contexts/AuthContext';
 
-const CommentSection = () => {
+const CommentSection = ({articleId}) => {
+    const { user, profile } = useAuth();
+    const [comments, setComments] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [newComment, setNewComment] = useState('');
+    const [submitting, setSubmitting] = useState(false);
+    const [editingId, setEditingId] = useState(null);
+    const [editText, setEditText] = useState('');
+    const commentInputRef = useRef(null);
+
+
+    // console.log("user info", user)
+
+
+
+
   return (
-    <div>CommentSection</div>
+    <div className=''>
+      
+    </div>
   )
 }
 
