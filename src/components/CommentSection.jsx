13b3414ec0b3b8a90d42ef8c8d0a43180713201e
Ahmed-abdisalaam-ahmed <shihabeldin70@gmail.com>
2026-01-03 18:00:46 +0000

    adding the comments part

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,5 +1,8 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useAuth } from '../contexts/AuthContext';
+import supabase from '../lib/superbase';
+import { FiMessageSquare, FiSend } from 'react-icons/fi';
+import { Link } from 'react-router';
 
 const CommentSection = ({articleId}) => {
     const { user, profile } = useAuth();
@@ -11,15 +14,103 @@ const CommentSection = ({articleId}) => {
     const [editText, setEditText] = useState('');
     const commentInputRef = useRef(null);
 
+    const handleSubmit = () => {
+      
+    }
 
     // console.log("user info", user)
 
+    const fetchComments = async () => {
+      try {
+        const {data , error} = await supabase
+              .from('comments')
+              .select(`*,
+                  user:userId (id , username , avatar_url)
+                `) 
+              .eq('article_id' ,articleId)
+              .order('created_at', { ascending: false });
 
+        if(error) throw error 
+        setComments(data || [])     
+      } catch (error) {
+        console.error('Error fetching comments: ', error)
+      }finally{
+        setLoading(false)
+      }
+    }
 
 
   return (
-    <div className=''>
-      
+    <div className='mt-10 bg-white p-5 rounded-lg border border-gray-200'>
+        <div className='flex items-center gap-2 mb-5'>
+           <FiMessageSquare className="text-gray-500 text-lg" />
+            <h2 className="text-xl font-medium text-gray-700">Comments ({comments.length})</h2>
+
+            {!user && (
+              <button 
+              // onClick={}
+              className='className="ml-auto px-3 py-1.5 text-sm bg-orange-500 text-white rounded-lg font-medium hover:bg-orange-600"'>
+                  join discussion 
+              </button>
+            )}
+        </div>
+        {user ? (
+            <form on onSubmit={handleSubmit} className="mb-6">
+                    <div className="flex gap-3">
+                        <img
+                            src={profile?.avatar_url || 'User'}
+                            alt={profile?.username || 'You'}
+                            className="w-8 h-8 rounded-full object-cover border border-gray-200 self-start mt-1"
+                        />
+
+                        <div className="flex-1">
+                            <textarea
+                                ref={commentInputRef}
+                                className="w-full p-3 border border-gray-200 rounded-lg bg-white text-gray-700 min-h-[100px]"
+                                placeholder="Share your thoughts..."
+                                value={newComment}
+                                onChange={(e) => setNewComment(e.target.value)}
+                                disabled={submitting}
+                            />
+                            <div className="flex justify-end mt-2">
+                                <button
+                                    type="submit"
+                                    className="px-4 py-2 bg-orange-500 text-white rounded-lg hover:bg-orange-600 disabled:opacity-50 flex items-center gap-2"
+                                // disabled={submitting || !newComment.trim()}
+                                >
+                                    {submitting ? 'Posting...' : 'Post Comment'}
+                                    <FiSend className={submitting ? 'opacity-0' : 'opacity-100'} />
+                                </button>
+                            </div>
+                        </div>
+
+                    </div>
+                </form>
+          ) : (
+            <div className="bg-gray-50 p-4 rounded-lg mb-6 border border-gray-200">
+                    <h3 className="text-base font-medium text-gray-700 mb-2">Join the conversation</h3>
+                    <p className="text-gray-600 mb-4 text-sm">
+                        Sign in to comment on this article.
+                    </p>
+
+
+                    <div className="flex gap-3">
+                        <Link
+                            to="/signin"
+                            className="px-4 py-2 bg-orange-500 hover:bg-orange-600 text-white rounded-lg text-sm font-medium"
+                        >
+                            Sign in
+                        </Link>
+                        <Link
+                            to="/signup"
+                            className="px-4 py-2 bg-white text-gray-700 border border-gray-300 rounded-lg text-sm font-medium hover:bg-gray-50"
+                        >
+                            Create account
+                        </Link>
+                    </div>
+                </div>
+          )
+        }
     </div>
   )
 }
